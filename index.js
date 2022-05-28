df9eb065692166f093b4337082e157e6d16e017d
nazibhaque137 <nazibhaque137@gmail.com>
2022-05-29 00:20:04 +0600

    get all items from items api

--- index.js
+++ index.js
@@ -14,12 +14,29 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sahbn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        await client.connect();
 
 const itemCollection = client.db("computerManufacturer").collection("item");
 const orderCollection = client.db('computerManufacturer').collection('order');
 const userCollection = client.db('computerManufacturer').collection('user');
 const reviewCollection = client.db("computerManufacturer").collection("review");
 
+
+        app.get('/item', async (req, res) => {
+            const query = {};
+            const cursor = itemCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
+        })
+    }
+
+    finally {
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', async (req, res) => {
     res.send('Computer Manufacturer Server running');
